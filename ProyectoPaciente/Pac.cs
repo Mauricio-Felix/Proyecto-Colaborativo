edf1e7aa49ea6a7fce61ac64ae47f5d28f3dca4c
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-08 23:06:50 -0500

    Nuevo paciente

---
+++ ProyectoPaciente/Pac.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPaciente
+{
+    public class Pac
+    {
+        public string Name { set; get; }
+
+        public Pac()
+        {
+
+        }
+
+        public Pac(string name)
+        {
+            Name = name;
+        }
+    }
+}
